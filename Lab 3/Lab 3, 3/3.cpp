330d8759b58f6e8a5f907b9c9dfb526baa106a1b
HolyMarble <56988397+HolyMarble@users.noreply.github.com>
2020-06-17 18:11:32 +0700

    Add files via upload

---
+++ Lab 3/Lab 3, 3/3.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<string.h>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	const int a = 3;
+	setlocale(LC_ALL, "");
+	const char p = '\n';
+	char temp_str[256];
+	const int len = 41, strings = 3;
+	char strng[strings][len];
+	ifstream fs("F:\\file.txt");
+	if (!fs) return 1; 
+	for (int r = 0; r < a; r++)
+	{
+		fs.getline(strng[r], len - 1, p); 
+	}
+	cout << "Упорядоченные предложения по убыванию их длины:\n";
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < a; j++)
+			if (strlen(strng[i]) > strlen(strng[j]))
+			{
+				strcpy(temp_str, strng[i]);
+				strcpy(strng[i], strng[j]);
+				strcpy(strng[j], temp_str);
+			}
+	}
+	for (int i = 0; i < a; i++)
+		cout << endl << strng[i] << endl;
+	cout << endl;
+	return 0;
+}
---
+++ Lab 3/Lab 3, q/2.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream> 
+#include <string>
+
+using namespace std;
+
+namespace SecondTask
+{
+	void FindTheShortestWord(char* MainStr, int min, char* BeforeChangesStr)
+	{
+		cout << MainStr;
+		for (char* p = strtok(MainStr, " "); p; p = strtok(NULL, " "))
+		{
+			if (strlen(p) < min)
+			{
+				min = strlen(p);
+				strcpy(BeforeChangesStr, p);
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int min = 25;
+	char MainStr[100];
+	char BeforeChangesStr[100];
+	char AllFileInOneArray[3][41];
+	const char p = '\n';
+	string path = "F:\\file.txt";
+	ifstream fin;
+	fin.open(path);
+	for (int i = 0; i < 3; i++)
+	{
+		fin.getline(AllFileInOneArray[i], 41 - 1, p);
+		cout << AllFileInOneArray[i] << "\n";
+	}
+	cout << endl;
+	strcpy(MainStr, AllFileInOneArray[0]);
+	SecondTask::FindTheShortestWord(MainStr, min, BeforeChangesStr);
+	cout << endl << "Самое короткое слово в строке: " << BeforeChangesStr;
+	return 0;
+}
+
